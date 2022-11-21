4adf6ec0ac867ff97dd94d4f7651f1cf0baf48eb
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-21 15:15:35 +0000

    moved operator function and different calculation functions to bottom of page, and commented out

--- index.js
+++ index.js
@@ -1,42 +1,3 @@
-// //ADD
-// const add = function(x, y){
-//     result = x + y;
-//     return result;
-// }
-// //SUBTRACT
-// const subtract = function(x, y){
-//     result = x - y;
-//     return result;
-
-// }
-// //MULTIPLY
-// const multiply = function(x, y) {
-//     result = x*y;
-//     return result;
-// }
-// //DIVIDE
-// const divide = function(x, y){
-//     result = x / y;
-//     return result;
-// }
-
-
-// //function depending on operator used
-// let operate = function(x, operator, y){
-
-// switch (operator) {
-//     case "+":
-//         result = add(x, y);
-//         break;
-//     case "-":
-//         result = subtract(x, y);
-//     case "/":
-//         result = divide(x, y);
-//     case "*":
-//         result = multiply(x, y)
-// }
-// return result
-// }
 
 //display of calculator
 let screenBelow = document.querySelector('.display-output').querySelector('.new-number');
@@ -127,3 +88,42 @@ buttons.map( button => {
 
 
 
+// //ADD
+// const add = function(x, y){
+//     result = x + y;
+//     return result;
+// }
+// //SUBTRACT
+// const subtract = function(x, y){
+//     result = x - y;
+//     return result;
+
+// }
+// //MULTIPLY
+// const multiply = function(x, y) {
+//     result = x*y;
+//     return result;
+// }
+// //DIVIDE
+// const divide = function(x, y){
+//     result = x / y;
+//     return result;
+// }
+
+
+// //function depending on operator used
+// let operate = function(x, operator, y){
+
+// switch (operator) {
+//     case "+":
+//         result = add(x, y);
+//         break;
+//     case "-":
+//         result = subtract(x, y);
+//     case "/":
+//         result = divide(x, y);
+//     case "*":
+//         result = multiply(x, y)
+// }
+// return result
+// }
