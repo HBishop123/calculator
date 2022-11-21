6fa48346bb5a540367b0ac7cae2a01f8263c9106
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-21 15:18:41 +0000

    removed changes for switch cases, as caused all numbers to change to +/-x0

--- index.js
+++ index.js
@@ -1,93 +1,3 @@
-
-//display of calculator
-let screenBelow = document.querySelector('.display-output').querySelector('.new-number');
-let screenAbove = document.querySelector('.display-output').querySelector('.previous-number')
-let buttons = Array.from(document.querySelectorAll('button'));
-
-
-buttons.map( button => {
-    button.addEventListener('click', (e) => {
-        switch(e.target.innerText){
-
-            case 'AC':
-                screenBelow.innerText = ''
-                screenAbove.innerText = ''
-                break;
-
-            case 'DEL':
-                screenBelow.innerText = screenBelow.innerText.slice(0, -1);
-                break;
-
-            case '.':
-                if (screenBelow.innerText.includes('.')){
-                screenBelow.innerText = screenBelow.innerText}    
-                else{screenBelow.innerText = screenBelow.innerText + '.'}
-                break;
-
-            default:
-                
-                screenBelow.innerText += e.target.innerText;
-                break;
-
-            case '+':
-               screenAbove.innerText = screenBelow.innerText + '+'
-               screenBelow.innerText = ''
-               if(screenAbove.innerText = '+'){
-               screenAbove.innerText = '0+'}
-            break;
-
-            case '/':
-               screenAbove.innerText = screenBelow.innerText + '/'
-               screenBelow.innerText = ''
-               if(screenAbove.innerText = '/'){
-               screenAbove.innerText = '0/'}
-            break;
-
-            case '-':
-               screenAbove.innerText = screenBelow.innerText + '-'
-               screenBelow.innerText = ''
-               if(screenAbove.innerText = '-'){
-               screenAbove.innerText = '0-'}
-            break;
-
-            case 'x':
-               screenAbove.innerText = screenBelow.innerText + 'x'
-               screenBelow.innerText = ''
-               if(screenAbove.innerText = '+'){
-               screenAbove.innerText = '0x'}v
-            break;
-
-            case '=':
-                let firstNumber = Number(screenAbove.innerText.slice(0, -1))
-                let secondNumber = Number(screenBelow.innerText)
-
-                if(screenAbove.innerText.includes('+')){
-                screenAbove.innerText = `${firstNumber} + ${secondNumber}`
-                screenBelow.innerText = firstNumber + secondNumber
-
-               }else if(screenAbove.innerText.includes('-')){
-               screenAbove.innerText = `${firstNumber} - ${secondNumber}`
-               screenBelow.innerText = firstNumber - secondNumber
-
-               }else if(screenAbove.innerText.includes('/')){
-               screenAbove.innerText = `${firstNumber} / ${secondNumber}`
-               screenBelow.innerText = firstNumber / secondNumber
-               if((firstNumber == 0) && (secondNumber == 0)){
-                return screenBelow.innerText = 'lol, nice try'
-               }
-               
-               }else if(screenAbove.innerText.includes('x')){
-                screenAbove.innerText = `${firstNumber} x ${secondNumber}`
-                screenBelow.innerText = firstNumber * secondNumber
-                }
-             }
-        
-    });
-});
-
-
-
-
 // //ADD
 // const add = function(x, y){
 //     result = x + y;
@@ -127,3 +37,85 @@ buttons.map( button => {
 // }
 // return result
 // }
+
+//display of calculator
+let screenBelow = document.querySelector('.display-output').querySelector('.new-number');
+let screenAbove = document.querySelector('.display-output').querySelector('.previous-number')
+let buttons = Array.from(document.querySelectorAll('button'));
+
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+
+            case 'AC':
+                screenBelow.innerText = ''
+                screenAbove.innerText = ''
+                break;
+
+            case 'DEL':
+                screenBelow.innerText = screenBelow.innerText.slice(0, -1);
+                break;
+
+            case '.':
+                if (screenBelow.innerText.includes('.')){
+                screenBelow.innerText = screenBelow.innerText}    
+                else{screenBelow.innerText = screenBelow.innerText + '.'}
+                break;
+
+            default:
+                
+                screenBelow.innerText += e.target.innerText;
+                break;
+
+            case '+':
+               screenAbove.innerText = screenBelow.innerText + '+'
+               screenBelow.innerText = ''
+            break;
+
+            case '/':
+               screenAbove.innerText = screenBelow.innerText + '/'
+               screenBelow.innerText = ''
+            break;
+
+            case '-':
+               screenAbove.innerText = screenBelow.innerText + '-'
+               screenBelow.innerText = ''
+            break;
+
+            case 'x':
+               screenAbove.innerText = screenBelow.innerText + 'x'
+               screenBelow.innerText = ''
+            break;
+
+            case '=':
+                let firstNumber = Number(screenAbove.innerText.slice(0, -1))
+                let secondNumber = Number(screenBelow.innerText)
+
+                if(screenAbove.innerText.includes('+')){
+                screenAbove.innerText = `${firstNumber} + ${secondNumber}`
+                screenBelow.innerText = firstNumber + secondNumber
+
+               }else if(screenAbove.innerText.includes('-')){
+               screenAbove.innerText = `${firstNumber} - ${secondNumber}`
+               screenBelow.innerText = firstNumber - secondNumber
+
+               }else if(screenAbove.innerText.includes('/')){
+               screenAbove.innerText = `${firstNumber} / ${secondNumber}`
+               screenBelow.innerText = firstNumber / secondNumber
+               if((firstNumber == 0) && (secondNumber == 0)){
+                return screenBelow.innerText = 'lol, nice try'
+               }
+               
+               }else if(screenAbove.innerText.includes('x')){
+                screenAbove.innerText = `${firstNumber} x ${secondNumber}`
+                screenBelow.innerText = firstNumber * secondNumber
+                }
+             }
+        
+    });
+});
+
+
+
+
