d1e300ebd07bfdddad8ec41bb57affe5aaf2eca8
Harrison Bishop <harrisonbishsp@gmail.com>
2022-11-21 11:48:09 +0000

    creating calculations on calculator. including + and -. When calculation is made, result shows on bottom line of display and calculation made is displayed on the top line of the display

--- index.js
+++ index.js
@@ -40,14 +40,59 @@ return result
 
 //display of calculator
 let screenBelow = document.querySelector('.display-output').querySelector('.new-number');
+let screenAbove = document.querySelector('.display-output').querySelector('.previous-number')
 let buttons = Array.from(document.querySelectorAll('button'));
+
 
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
         switch(e.target.innerText){
+
+            case 'AC':
+                screenBelow.innerText = ''
+                screenAbove.innerText = ''
+                break;
+
+            case 'DEL':
+                screenBelow.innerText = screenBelow.innerText.slice(0, -1);
+                break;
+            
             default:
-                screenBelow.innerText += e.target.innerText
+                
+                screenBelow.innerText += e.target.innerText;
+                break;
+
+            case '+':
+                screenAbove.innerText = screenBelow.innerText + '+'
+                screenBelow.innerText = ''
+                break;
+
+            case '/':
+               screenAbove.innerText = screenBelow.innerText + '/'
+               screenBelow.innerText = ''
+            break;
+
+            case '-':
+               screenAbove.innerText = screenBelow.innerText + '-'
+               screenBelow.innerText = ''
+            break;
+
+            case '=':
+                let firstNumber = Number(screenAbove.innerText.slice(0, -1))
+                let secondNumber = Number(screenBelow.innerHTML)
+
+                if(screenAbove.innerText.includes('+')){
+                screenAbove.innerText = `${firstNumber} + ${secondNumber}`
+                screenBelow.innerText = firstNumber + secondNumber
+
+               }else if(screenAbove.innerText.includes('-')){
+               screenAbove.innerText = `${firstNumber} - ${secondNumber}`
+               screenBelow.innerText = firstNumber - secondNumber
+               }
+
+
+
         }
         
     });
